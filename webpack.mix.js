d90f4c58fd706ea4ad81d627ecb2cd81931a4673
rklugman <rwklugman@gmail.com>
2022-04-11 12:33:17 +0200

    Nova 4

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,18 @@
 let mix = require('laravel-mix')
+let path = require('path')
 
-mix
+mix.alias({
+  'laravel-nova': path.join(__dirname, '../../vendor/laravel/nova/resources/js/mixins/packages.js'),
+})
+
+mix.js('resources/js/filter.js', 'js').vue({ version: 3 })
+  .webpackConfig({
+    externals: {
+      vue: 'Vue',
+    },
+    output: {
+      uniqueName: 'vendor/nova-multiselect-filter',
+    }
+  })
   .setPublicPath('dist')
-  .js('resources/js/filter.js', 'js')
+
